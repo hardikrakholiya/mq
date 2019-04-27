e56d7fe3edc1fb6c4cda8b6c0675a6c31501adbb
aishwaryadhage <aishwarya.dhage95@gmail.com>
2019-04-27 18:25:05 -0400

    added wrapper class

--- gateway_api.py
+++ gateway_api.py
@@ -1,16 +1,33 @@
-def pub(publisher, msg):
-    publisher.send_json(msg)
-    ack = publisher.recv()
-    return ack
+import zmq
+import time
 
 
-offset = 0
+class gateway_api:
+    def __init__(self, host, port, topic):
+        self.topic = topic
+        self.host = host
+        self.port = port
+        self.offset = 0
+        self.context = zmq.Context()
+        self.socc = self.context.socket(zmq.REQ)
+        self.socc.connect("tcp://"+host+":"+port)
 
+    def pub(self, msg):
+        json_msg = {}
+        json_msg["timestamp"] = time.time()
+        json_msg["topic"] = self.topic
+        json_msg["message"] = msg
+        self.socc.send_json(json_msg)
+        ack = self.socc.recv()
+        return ack
 
-def sub(publisher, msg):
-    global offset
-    msg["offset"] = offset
-    publisher.send_json(msg)
-    msg = publisher.recv()
-    offset += 1
-    return msg
+    def sub(self):
+        global offset
+        json_msg = {}
+        json_msg["offset"] = self.offset
+        json_msg["topic"] = self.topic
+        self.socc.send_json(json_msg)
+        msg = self.socc.recv()
+        print self.offset
+        self.offset += 1
+        return msg
--- sub.py
+++ sub.py
@@ -1,30 +1,17 @@
 import time
-import zmq
 from threading import Thread
-from gateway_api import pub, sub
-
-
-msg = {
-    "topic": "Queue-1"
-}
+import gateway_api as ga
 
 
 def main():
-    # Prepare our context and publisher
-    context = zmq.Context()
-    publisher = context.socket(zmq.REQ)
-    publisher.connect("tcp://localhost:5566")
-
-    # sendgtwy = gateway_api.gateway()
-    ack = sub(publisher, msg)
-
-    # offset = msg["offset"]
+    host = "localhost"
+    port = "5566"
+    topic = "Queue-1"
+    socke = ga.gateway_api(host, port, topic)
+    ack = socke.sub()
     print ack
-
-    publisher.close()
-    context.term()
 
 
 if __name__ == "__main__":
     Thread(target=main).start()
-    Thread(target=main).start()
+    # Thread(target=main).start()
