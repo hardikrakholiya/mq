a74802bacd31f66255d48b1fc3877a430c0ed403
Siddharth Pathak <siddharthpathak7@gmail.com>
2019-04-27 21:19:26 -0400

    Some minor changes

--- gateway_server.py
+++ gateway_server.py
@@ -4,47 +4,50 @@ import zmq
 from threading import Thread
 
 
-def connecttobroker(message):
+def connect_to_broker(url, port, message):
     con = zmq.Context()
     socc = con.socket(zmq.REQ)
-    socc.connect("tcp://149.160.227.28:5555")
+    socc.connect("tcp://"+url+":"+port)
     print "connected to broker"
     socc.send_json(message)
-    # time.sleep(1)
     ack = socc.recv()
     return ack
 
 
-def worker_routine(worker_url, gtwy_name, context=None):
+def worker_routine(url_worker, port):
     """Worker routine"""
-    context = context or zmq.Context.instance()
+    context = zmq.Context.instance()
     # Socket to talk to dispatcher
     socket = context.socket(zmq.REP)
-    socket.connect(worker_url)
+    socket.connect(url_worker)
     while True:
         message = socket.recv_json()
-        print "message received"
-        ack = connecttobroker(message)
-
-        print "Received on"+gtwy_name+": ", ack
+        # This will be fetched from ZK 
+        url = "149.160.227.28"
+        port = 5555
+        ack = connect_to_broker(url, port, message)
         socket.send_json(ack)
 
 
 def delegatejob(port, gtwy_name):
     url_worker = "inproc://workers"+gtwy_name
     url_gtwy = "tcp://*:"+port
+
     # Prepare our context and sockets
     context = zmq.Context.instance()
+
     # Socket to talk to clients
     gtwy = context.socket(zmq.ROUTER)
     gtwy.bind(url_gtwy)
+
     # Socket to talk to workers
     workers = context.socket(zmq.DEALER)
     workers.bind(url_worker)
+
     # Launch pool of worker threads
     for i in range(5):
         thread = Thread(
-            target=worker_routine, args=(url_worker, gtwy_name))
+            target=worker_routine, args=(url_worker,port))
         thread.start()
 
     zmq.proxy(gtwy, workers)
@@ -53,16 +56,16 @@ def delegatejob(port, gtwy_name):
     context.term()
 
 
-def prod_gtwy():
-    delegatejob("5565", "prod_gtwy")
+def start_prod_gateway():
+    delegatejob("5565", "prod_gateway")
 
 
-def cons_gtwy():
-    delegatejob("5566", "con_gtwy")
+def start_cons_gateway():
+    delegatejob("5566", "cons_gateway")
 
 
 if __name__ == "__main__":
     # zk = KazooClient(hosts='localhost:2181', read_only=True)
     # zk.start()
-    Thread(target=prod_gtwy).start()
-    Thread(target=cons_gtwy).start()
+    Thread(target=start_prod_gateway).start()
+    Thread(target=start_cons_gateway).start()
